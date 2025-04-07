ccde03f9995640ae3e4732e43e55832116e86d83
root <anup.s.bhoos@gmail.com>
2025-04-07 17:46:48 +0000

    change settings

--- PersonalDrive/settings.py
+++ PersonalDrive/settings.py
@@ -28,8 +28,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"  # For Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "anup.s.bhoos@gmail.com"
-EMAIL_HOST_PASSWORD = "hfuw gjfc lsft rbph" 
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
